00434bf9caab749fd9ca07c8e217bc1e3a025060
Saidulu Alli <saidulu.alli@gmail.com>
2016-05-06 09:26:13 -0700

    Error code

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -28,6 +28,7 @@ public enum Cell {
     public String getSymbol() {
         // Just adding a printl message
 	System.out.println("In Method : getSymbol");
+        System.out.printlsn("Hi This is Sai");
         return symbol;
     }
 }
